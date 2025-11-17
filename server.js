cdbd5dc4de927ca21393f682af0ed4bf5ebfc999
Mahesh_Bharambe <144551859+Maheshbharambe45@users.noreply.github.com>
2025-11-17 22:03:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello from Node.js + Docker + Kubernetes + Jenkins CI/CD!");
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-
-module.exports = app; // <-- Added for Jest testing
+module.exports = app; 
